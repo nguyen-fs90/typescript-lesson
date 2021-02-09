b4bfee97b58f013a581fcfabadf3fbbbe92ce968
Nguyen Manh Hiep <nguyen.fs90@gmail.com>
2021-02-09 09:10:21 +0900

    basic types

--- src/BasicTypes.ts
+++ src/BasicTypes.ts
@@ -1,5 +1,46 @@
 /**
- * boolean
+ * Boolean
  */
 let isDone: boolean = false;
-console.log(isDone);
+
+/**
+ * Number
+ */
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+// trên vscode báo lỗi khi khai báo kiểu biginit
+// let big: bigint = 100n;
+
+/**
+ * String
+ */
+let color: string = "blue";
+color = "red";
+
+let fullName: string = `Bob Bobbing`;
+let age: number = 37;
+let sentence: string = `Hello, my name is ${fullName}. I'll be ${age + 1} years old next month`;
+
+/**
+ * Array
+ */
+let listA: number[] = [1,2,3];
+let listB: Array<number> = [1,2,3];
+
+/**
+ * Tuple
+ */
+let x: [string, number];
+x = ["hello", 10];
+
+/**
+ * Enum
+ */
+enum Color {
+  Red,
+  Green,
+  Blue,
+}
+let c: Color = Color.Green;
