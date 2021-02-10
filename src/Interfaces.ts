fdcd0489e3c81d458dea6e5fecfa003d03f1b3d2
Nguyen Manh Hiep <nguyen.fs90@gmail.com>
2021-02-10 18:11:04 +0900

    interfaces

---
+++ src/Interfaces.ts
@@ -0,0 +1,38 @@
+/**
+ * Out First Interface
+ */
+interface LabeledValue {
+  label: string;
+}
+
+function printLabel(labeledObj: LabeledValue) {
+  console.log(labeledObj.label);  
+}
+
+let myObj = {size: 10, label: "Size 10 Object"};
+printLabel(myObj);
+
+/**
+ * Optional Properties
+ */
+interface SquareConfig {
+  color?: string;
+  width?: number;
+}
+
+function createSquare(config: SquareConfig): {color: string; area: number} {
+  let newSquare = {color: "white", area: 100};
+  if (config.color) {
+    newSquare.color = config.color;
+  }
+  if (config.width) {
+    newSquare.area = config.width * config.width;
+  }
+  return newSquare;
+}
+
+let mySquare = createSquare({color: "black"});
+
+/**
+ * Readonly properties
+ */
