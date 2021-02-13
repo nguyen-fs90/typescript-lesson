2d1a1784f6e91a167cd240d5489e17dce45ebace
Nguyen Manh Hiep <nguyen.fs90@gmail.com>
2021-02-13 19:00:40 +0900

    interface types

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -36,3 +36,101 @@ let mySquare = createSquare({color: "black"});
 /**
  * Readonly properties
  */
+interface Point {
+  readonly x: number;
+  readonly y: number;
+}
+
+/**
+ * Excess Property Checks
+ */
+interface SquareConfig {
+  color?: string;
+  width?: number;
+}
+
+/**
+ * Function Types
+ */
+interface SearchFunc {
+  (source: string, subString: string): boolean;
+}
+
+let mySearch: SearchFunc;
+mySearch = function (source: string, subString: string) {
+  let result = source.search(subString);
+  return result > -1;
+};
+
+/**
+ * Indexable Types
+ */
+interface StringArray {
+  [index: number]: string;
+}
+let myArray: StringArray;
+myArray = ["Bob", "Fred"];
+
+let myStr: string = myArray[0];
+
+/**
+ * Class Types
+ */
+interface ClockInterface {
+  currentTime: Date;
+}
+
+class Clock implements ClockInterface {
+  currentTime: Date = new Date();
+  constructor(h: number, m: number) {}
+}
+
+/**
+ * Extending Interfaces
+ */
+interface Shape {
+  color: string;
+}
+
+interface Square extends Shape {
+  sideLength: number;
+}
+
+let square = {} as Square;
+square.color = "blue";
+square.sideLength = 10;
+
+/**
+ * Hybrid Types
+ */
+interface Counter {
+  (start: number): string;
+  interval: number;
+  reset(): void;
+}
+
+function getCounter(): Counter {
+  let counter = function (start: number) {} as Counter;
+  counter.interval = 123;
+  counter.reset = function () {};
+  return counter;
+}
+
+/**
+ * Interfaces Extending Classes
+ */
+class Control {
+  private state: any;
+}
+
+interface SelectableControl extends Control {
+  select(): void;
+}
+
+class Button extends Control implements SelectableControl {
+  select() {}
+}
+
+class TextBox extends Control {
+  select() {}
+}
