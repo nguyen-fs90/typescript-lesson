14ac0f50a37c4ce79c1be90148050162cb3b2dc0
Nguyen Manh Hiep <nguyen.fs90@gmail.com>
2021-02-20 20:30:22 +0900

    enums types

---
+++ src/Enums.ts
@@ -0,0 +1,19 @@
+/**
+ * Numeric enums
+ */
+enum DirectionNumber {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+/**
+ * String enums
+ */
+enum DirectionString {
+  Up = "UP",
+  Down = "DOWN",
+  Left = "LEFT",
+  Right = "RIGHT",
+}
